9a903b27efd4a40d357422d8cc25a447373c266a
roodea <roodea@storewtk.com>
2021-09-09 17:25:33 +0800

    测试写入链与币信息

--- samples/main.go
+++ samples/main.go
@@ -3,12 +3,67 @@ package main
 import (
 	"fmt"
 	"github.com/xiaoyueya/chain-sdk/common"
+	"github.com/xiaoyueya/chain-sdk/services"
 )
 
+/**
+"redis_config":{
+    "Addr":"127.0.0.1:6379",
+    "DB":1,
+    "DialTimeout":10,
+    "ReadTimeout":30,
+    "WriteTimeout":30,
+    "PoolSize":10,
+    "PoolTimeout":30,
+    "IdleTimeout":500,
+    "IdleCheckFrequency":500,
+    "Password":""
+  },
+**/
 func main() {
-	chainInfo := common.ChainInfo{}
-	fmt.Printf("chain info=%v\n", chainInfo)
-	coinInfo := common.CoinOrToken{}
-	fmt.Printf("coin info=%v\n", coinInfo)
+	err := services.InitRedisService(&common.RedisConfig{
+		Addr:               "127.0.0.1:6379",
+		DB:                 0,
+		DialTimeout:        10,
+		ReadTimeout:        30,
+		WriteTimeout:       30,
+		PoolSize:           10,
+		PoolTimeout:        30,
+		IdleTimeout:        500,
+		IdleCheckFrequency: 500,
+		Password:           "654123",
+	})
 
+	if err != nil {
+		panic(err)
+	}
+
+	chainInfo := &common.ChainInfo{
+		Chain:                   "FTM",
+		ChainID:                 "250",
+		RPCUrls:                 []string{"https://rpc.ftm.tools/"},
+		Symbol:                  "FTM",
+		BlockExplorerAddressURL: "https://ftmscan.com/address/%s",
+		BlockExplorerTxURL:      "https://ftmscan.com/tx/%s",
+	}
+	fmt.Printf("chain info=%v\n", chainInfo)
+	err = services.AddOrUpdateChain(chainInfo)
+	if err != nil {
+		panic(err)
+	}
+	coinInfo := &common.CoinOrToken{
+		Chain:           "FTM",
+		ChainId:         "250",
+		CustomName:      "FRC20USDC",
+		TokenName:       "USD coin",
+		Symbol:          "USDC",
+		Decimals:        6,
+		ContractAddress: "0x04068da6c83afcfa0e13ba15a6696662335d5b75",
+		IsXXX20:         true,
+	}
+	err = services.AddOrUpdateCoin(coinInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("coin info=%v\n", coinInfo)
 }
