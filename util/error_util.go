cc8319cda9e33d75d5b60d377ff6b8d8a2dafb4a
roodea <roodea@storewtk.com>
2021-10-26 16:38:38 +0800

    添加请求ip限制错误"403 Forbidden"的case

--- util/error_util.go
+++ util/error_util.go
@@ -6,5 +6,9 @@ func IsTooManyRequestError(err error) bool {
 	if strings.Contains(strings.ToLower(err.Error()), "too many") {
 		return true
 	}
+	if strings.Contains(strings.ToLower(err.Error()), "403 forbidden") {
+		return true
+	}
+
 	return false
 }
